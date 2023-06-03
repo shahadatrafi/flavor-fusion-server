714191fbfbe270f645f969ddbdb1e0628820723a
ShahadatRafi <mdshahadathossenrafi@gmail.com>
2023-06-04 00:54:37 +0600

    delete api added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express()
 const port = process.env.PORT || 5000;
@@ -55,6 +55,12 @@ async function run() {
             const query = { email: email }
             const result = await cartCollection.find(query).toArray();
             res.send(result);
+        })
+
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(query)
         })
 
         app.post('/carts', async(req, res) => {
